0d313b509162f76db4b6ccffe127974a6a3ff125
Mitali <mitali9205@gmail.com>
2025-07-08 22:39:41 +0530

    fix: responsiveness

--- frontend/src/components/shared/Footer.tsx
+++ frontend/src/components/shared/Footer.tsx
@@ -1,9 +1,9 @@
 
 export const Footer = () => {
   return (
-    <footer className="container mx-auto px-4 py-4 mt-20 border-t">
+    <footer className="container mx-auto px-4 py-4 mt-20 border-t ">
       <div className="flex justify-center items-center gap-2 md:gap-4 typography">
-        <div className="w-16 h-8 flex items-center">
+        <div className="w-16 h-8 flex items-center select-none">
           <img
             src="/logo-2.svg"
             alt="logo"
