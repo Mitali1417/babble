029a5175ea395187dbee0a677a34ad194bb1bf13
Mitali <mitali9205@gmail.com>
2025-07-09 20:49:27 +0530

    fix: footer

--- frontend/src/components/shared/Footer.tsx
+++ frontend/src/components/shared/Footer.tsx
@@ -1,16 +1,10 @@
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
     <footer className="container mx-auto px-4 py-4 mt-20 border-t ">
       <div className="flex justify-center items-center gap-2 md:gap-4 typography">
-        <div className="w-16 h-8 flex items-center select-none">
-          <img
-            src="/logo-2.svg"
-            alt="logo"
-            className="w-full h-full dark:invert"
-          />
-        </div>
-        <p>&copy; 2025 babble. Made with ❤️ by Mitali</p>
+        <p>&copy; 2025 babble. Made with ❤️ by <Link to="https://www.linkedin.com/in/mitalis14/" target="_blank">Mitali</Link></p>
       </div>
     </footer>
   );
